0670be4799b1c360cf9aac9d87e81cc6b8264e51
Matheus Freire Bezerra <matheus.freire.bezerra@gmail.com>
2023-03-24 00:08:08 -0300

    commit consertando a validaçao da senha e comentando o css

--- Formulario da faculdade/script.js
+++ Formulario da faculdade/script.js
@@ -17,35 +17,37 @@ function checkInputs(){ //verificando os inputs
     const passwordConfirmationValue = passwordConfirmation.value
     const specialCharRegex = /[-@!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/;
     const numberRegex = /\d/;
-
+    //validando os dados
     if(usernameValue === ''){
-        setErrorFor(username, "O nome de usuário é obrigatório")
+        setErrorFor(username, "O nome de usuário é obrigatório.")
     }else{
         setSuccessFor(username)
     }
 
     if(emailValue=== ""){
-        setErrorFor(email,"O email é obrigatório")
+        setErrorFor(email,"O email é obrigatório.")
     }else if(!checkEmail(emailValue)){
-        setErrorFor(email,"Insira um email válido")
+        setErrorFor(email,"Insira um email válido.")
     }else{
         setSuccessFor(email)
     }
 
     if(passwordValue===""){
-        setErrorFor(password,"A senha é obrigatória")
+        setErrorFor(password,"A senha é obrigatória.")
     }else if(passwordValue.length < 7){
         setErrorFor(password,"A senha deve ter no minímo 8 caracteres.")
+    }else if(!specialCharRegex.test(password.value) && !numberRegex.test(password.value)){
+        setErrorFor(password,'A senha deve ter no minímo um caractere especial e um número.')
     }else if(!specialCharRegex.test(password.value)){
-        setErrorFor(password,"A senha tem que ter no minímo um caractere especial")
+        setErrorFor(password,"A senha tem que ter no minímo um caractere especial.")
     }else if(!numberRegex.test(password.value)){
-        setErrorFor(password,"A senha tem que ter no minímo um número")
+        setErrorFor(password,"A senha tem que ter no minímo um número.")
     }else{
         setSuccessFor(password)
     }
 
     if(passwordConfirmationValue===""){
-        setErrorFor(passwordConfirmation, 'A confirmação da senha é obrigatório')
+        setErrorFor(passwordConfirmation, 'A confirmação da senha é obrigatório.')
     }else if(passwordConfirmationValue!=passwordValue){
         setErrorFor(passwordConfirmation, "As senhas não conferem.")
     }else{
@@ -61,7 +63,6 @@ function checkInputs(){ //verificando os inputs
     if (formIsValid) {  
       console.log("O formulário está válido")
       document.getElementById("botaoCadastro").addEventListener("click", function() { //quando o formulario for valido ira para a pagina de login
-        alert('Dados válidos! Faça login agora.')
         window.location.href = "pagLogin.html";
       });
     }
