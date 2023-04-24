c5ff449eaf7e64bee9aeb79ebab6865836629544
Matheus Freire Bezerra <101263874+matheusbezerraa@users.noreply.github.com>
2023-04-24 12:13:42 -0300

    concertando a validação do password

--- Formulario da faculdade/logincode.js
+++ Formulario da faculdade/logincode.js
@@ -6,11 +6,17 @@ form.addEventListener('submit', (e)=>{
     checkInputs()
 })
 
+function logar(){
+    localStorage.setItem("username", document.querySelector("#username").value)
+    localStorage.setItem("password", document.querySelector("#password").value)
+}
+
 function checkInputs(){
+    const storedData = localStorage.getItem('userData');
     const usernameValue = username.value
     const passwordValue = password.value
-    const numberRegex = /\d/
     const specialCharRegex = /[-@!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/;
+    const numberRegex = /\d/;
     
     if(usernameValue === ''){
         setErrorFor(username, "O nome de usuário é obrigatório.")
@@ -20,6 +26,14 @@ function checkInputs(){
 
     if(passwordValue===""){
         setErrorFor(password,"A senha é obrigatória.")
+    }else if(passwordValue.length < 7){
+        setErrorFor(password,"A senha deve ter no minímo 8 caracteres.")
+    }else if(!specialCharRegex.test(password.value) && !numberRegex.test(password.value)){
+        setErrorFor(password,'A senha deve ter no minímo um caractere especial e um número.')
+    }else if(!specialCharRegex.test(password.value)){
+        setErrorFor(password,"A senha tem que ter no minímo um caractere especial.")
+    }else if(!numberRegex.test(password.value)){
+        setErrorFor(password,"A senha tem que ter no minímo um número.")
     }else{
         setSuccessFor(password)
     }
@@ -29,13 +43,16 @@ function checkInputs(){
     const formIsValid = [...formControls].every((formControl) => {
         return formControl.className === "form-control success"
       })
-      if (formIsValid) {  
-        //quando o formulario for valido ira para a pagina da landing page
+      if (formIsValid) {
+        //quando o formulario for valido ira para a landing page
         setTimeout(() => {
-          alert("Login completo!")
-          window.location.href = "landingPage.html";
-        }, 1000); 
+            alert("Login completo!")
+            window.location.href = "landingPage.html";
+          }, 1000); 
+      }else{     
+        alert("erro")
       }
+      
   }
   function setErrorFor(input, message){
     const formControl = input.parentElement
